0b9076f3d470776bd266e9bb59fcb003372f9ee4
ruvesh <ruvesh2145@gmail.com>
2020-08-22 21:42:47 +0530

    Spring Boot Starter

---
+++ src/main/java/in/vruvesh/learningprojects/restfileuploaddownload/RestFileUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package in.vruvesh.learningprojects.restfileuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFileUploadDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFileUploadDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/vruvesh/learningprojects/restfileuploaddownload/RestFileUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vruvesh.learningprojects.restfileuploaddownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFileUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
