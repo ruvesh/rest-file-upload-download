2d134efe7eacef3a66939d249c91e0003f810d43
ruvesh <ruvesh2145@gmail.com>
2020-08-24 00:16:15 +0530

    Added cross Origin

--- src/main/java/in/vruvesh/learningprojects/restfileuploaddownload/controller/FilesController.java
+++ src/main/java/in/vruvesh/learningprojects/restfileuploaddownload/controller/FilesController.java
@@ -10,10 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
@@ -22,6 +19,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @Log4j2
+@CrossOrigin("*")
 public class FilesController {
 
     @Autowired
